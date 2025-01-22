a0b142580acc4a541c7d64f1925ea8a048dfe12b
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-01-22 14:12:28 +0200

    Added a simple view

---
+++ lib/homepage.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Counter in SharedPrefs'),
+      ),
+      body: Center(
+        child: Text('Counter App'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+        backgroundColor: Colors.blue,
+      ),
+    );
+  }
+}
