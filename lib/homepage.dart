02cc25e9acbe3ffab6da4d0b034546dafd417823
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-01-22 14:58:59 +0200

    Finished making cool changes to the app, sharedPreferences are working

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,21 +1,106 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _value = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadCounter();
+  }
+
+  void _loadCounter() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _value = (prefs.getInt('counter') ?? 0);
+    });
+  }
+
+  void _incrementCounter() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    setState(() {
+      _value = (prefs.getInt('counter') ?? 0) + 1;
+      prefs.setInt('counter', _value);
+    });
+  }
+
+  void _decrementCounter() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    setState(() {
+      _value = (prefs.getInt('counter') ?? 0) - 1;
+      prefs.setInt('counter', _value);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text('Counter in SharedPrefs'),
-      ),
-      body: Center(
-        child: Text('Counter App'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.add),
+        title: Text(
+          'Counter in SharedPrefs',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
         backgroundColor: Colors.blue,
+        elevation: 0,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Text(
+              '$_value',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 40,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            spacing: 10,
+            children: [
+              ElevatedButton(
+                onPressed: _incrementCounter,
+                child: Text(
+                  'Add',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                style: ButtonStyle(
+                    backgroundColor: WidgetStatePropertyAll(Colors.blue)),
+              ),
+              ElevatedButton(
+                onPressed: _decrementCounter,
+                child: Text(
+                  'Subtract',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                style: ButtonStyle(
+                    backgroundColor: WidgetStatePropertyAll(Colors.blue)),
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
