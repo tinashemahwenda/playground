c9568ba984d67f97a3e78c339e59deb34f01c152
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-01-23 06:31:52 +0200

    Setup a function to check if the name exists or not

--- lib/setup_page.dart
+++ lib/setup_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:playground/homepage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SetupPage extends StatefulWidget {
   const SetupPage({super.key});
@@ -14,6 +15,23 @@ class _SetupPageState extends State<SetupPage> {
   final _phoneNumberController = TextEditingController();
   bool _isFormVisible = true;
   String _existingName = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _checkExistingName();
+  }
+
+  _checkExistingName() async {
+    final prefs = await SharedPreferences.getInstance();
+    final existingName = prefs.getString('name');
+    if (existingName != null) {
+      setState(() {
+        _existingName = existingName;
+        _isFormVisible = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
