fe7d3837878072daea7eeaa5945aaf00c7033960
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-01-23 11:54:40 +0200

    Added data and a selection filter feature

--- lib/questions_page.dart
+++ lib/questions_page.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
 
-class QuestionsPage extends StatelessWidget {
+class QuestionsPage extends StatefulWidget {
   const QuestionsPage({super.key});
 
   @override
+  State<QuestionsPage> createState() => _QuestionsPageState();
+}
+
+class _QuestionsPageState extends State<QuestionsPage> {
+  final List<Map<String, dynamic>> questions = [
+    {'year': 2020, 'paperNumber': 1, 'body': 'Question 1 from 2020'},
+    {'year': 2020, 'paperNumber': 2, 'body': 'Question 1 from 2020'},
+    {'year': 2021, 'paperNumber': 1, 'body': 'Question 1 from 2021'},
+    {'year': 2021, 'paperNumber': 2, 'body': 'Question 1 from 2021'},
+    {'year': 2022, 'paperNumber': 1, 'body': 'Question 1 from 2022'}
+  ];
+
+  String selectedFilter = 'All';
+
+  @override
   Widget build(BuildContext context) {
+    List<Map<String, dynamic>> filteredQuestions = selectedFilter == 'All'
+        ? questions
+        : questions
+            .where((q) => q['year'].toString() == selectedFilter)
+            .toList();
     return Scaffold(
       primary: true,
       appBar: AppBar(
