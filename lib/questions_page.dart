414b6379e7e6454d7d432109311da0b131ddb51b
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-01-23 11:43:44 +0200

    Added a simple view for questions

---
+++ lib/questions_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class QuestionsPage extends StatelessWidget {
+  const QuestionsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Center(
+            child: Text('Questions here'),
+          )
+        ],
+      ),
+    );
+  }
+}
