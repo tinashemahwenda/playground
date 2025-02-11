51221b649679f33b61d0ef4741295c67e43dd79b
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-02-11 16:48:36 +0200

    Created a cool clock

---
+++ lib/digital_clock.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class DigitalClock extends StatefulWidget {
+  const DigitalClock({super.key});
+
+  @override
+  State<DigitalClock> createState() => _DigitalClockState();
+}
+
+class _DigitalClockState extends State<DigitalClock> {
+  late String _timeString;
+
+  @override
+  void initState() {
+    super.initState();
+    _updateTime();
+    Timer.periodic(Duration(seconds: 1), (Timer t) => _updateTime());
+  }
+
+  void _updateTime() {
+    final now = DateTime.now();
+    setState(() {
+      _timeString = "${now.hour.toString().padLeft(2, '0')}:"
+          "${now.minute.toString().padLeft(2, '0')}:"
+          "${now.second.toString().padLeft(2, '0')}";
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text(_timeString));
+  }
+}
