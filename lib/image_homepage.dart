af95b63f8e2053372559b1d4e512211c13cd67e4
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-02-12 20:03:33 +0200

    Creatd a simple view for the image page

---
+++ lib/image_homepage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ImageHomepage extends StatelessWidget {
+  const ImageHomepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [],
+    );
+  }
+}
